f9f61664231bf83710f92fe1da97258daf7a3556
AmirrezaAhmadi <AmirrezaAhmadi.Gh@gmail.com>
2024-09-15 23:59:59 +0330

    javaScript added

---
+++ Assets/js/javascript.js
@@ -0,0 +1,35 @@
+const generatePassword = () => {
+    const length = document.getElementById("length").value;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSymbols = document.getElementById("symbols").checked;
+
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_+";
+
+    let charset = "";
+    if (includeLowercase) charset += lowercase;
+    if (includeUppercase) charset += uppercase;
+    if (includeNumbers) charset += numbers;
+    if (includeSymbols) charset += symbols;
+
+    const warning = document.getElementById("warning");
+
+    if (!charset) {
+        warning.style.display = "block";
+        return;
+    } else {
+        warning.style.display = "none";
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
